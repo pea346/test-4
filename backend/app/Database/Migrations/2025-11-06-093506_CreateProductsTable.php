93dff977ce5182a5c264f9fb45acc7c94df9bae6
Joshua Ivan Reyes <joshuaivanp.reyes@gmail.com>
2025-11-06 18:35:10 +0800

    feat(user table): Added a migration for users table and addtional table

---
+++ backend/app/Database/Migrations/2025-11-06-093506_CreateProductsTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'description' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+            'price' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+                'null'       => false,
+            ],
+            'item' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('products', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products', true);
+    }
+}
